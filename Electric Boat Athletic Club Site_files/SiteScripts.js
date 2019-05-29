3f4662d63c973b00994f9736cd71db1ef85106df
enorman96 <emmacnorman@hotmail.com>
2019-05-29 19:49:08 -0400

    Separate style (.css) and scripts (.js)
    html, css, and javascript are now in separate files

---
+++ Electric Boat Athletic Club Site_files/SiteScripts.js
@@ -0,0 +1,18 @@
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+/* Mountain project data API: https://www.mountainproject.com/data. Also look at google map API for route destimation overlay */
+
