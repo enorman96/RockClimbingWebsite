492f4fccf3a7508a1f6819febfd433e2a8937fc1
enorman96 <emmacnorman@hotmail.com>
2020-03-24 22:51:46 -0400

    Navbar hides upon scroll, color/header style changes
    - removed/combined redundant/superfluous css code (combined functions)o
    - changed background from green to off-white
    - navbar navigates to desired sections
    - header edited to be larger

--- SiteScripts.js
+++ SiteScripts.js
@@ -1,17 +1,28 @@
+// 
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+//
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.maxHeight){
+//       content.style.maxHeight = null;
+//     } else {
+//       content.style.maxHeight = content.scrollHeight + "px";
+//     }
+//   });
+// }
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
 }
-
 /* Mountain project data API: https://www.mountainproject.com/data. Also look at google map API for route destimation overlay */
